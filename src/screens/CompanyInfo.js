dddd04bd65ebffc782eb2ed86efb03519ffce4f2
saltafinalatix <saltanat.k@finalatix.com>
2021-08-17 15:27:17 +0600

    companyinfo

--- src/screens/CompanyInfo.js
+++ src/screens/CompanyInfo.js
@@ -2,47 +2,42 @@ import React, { useState } from 'react';
 import { Paper, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import CompanyInfoForm from './CompanyInfoForm';
+import WalletDetailsForm from './WalletDetailsForm';
 
 const useStyles = makeStyles(() => ({
     companyinfocard: {
         display: 'flex',
+        marginRight: 10,
         marginTop: 20,
         marginLeft: 20
     },
     paper: {
+        marginLeft: 25,
         padding: 20,
-        width: 500
+        width: 600
     },
     paperheader: {
         background: '#edf2fb',
-        padding: 10
+        padding: 10,
+        textTransform: 'uppercase'
     }
-
-    // companyinfo: {
-    //     padding: 20,
-    //     marginRight: 23
-    // },
-
-    // cardheader: {
-    //     background: '#edf2fb'
-    // },
 }));
 
 const CompanyInfo = () => {
     const classes = useStyles();
     return (
         <div className={classes.companyinfocard}>
-            {/* <Card className={classes.companyinfo}>
-                <CardHeader
-                    className={classes.cardheader}
-                    title="Company Info"
-                />
-            </Card> */}
             <Paper className={classes.paper}>
                 <Typography className={classes.paperheader} variant="h4">
                     Company Info
                 </Typography>
                 <CompanyInfoForm />
+            </Paper>
+            <Paper className={classes.paper}>
+                <Typography className={classes.paperheader} variant="h4">
+                    Wallet Details
+                </Typography>
+                <WalletDetailsForm />
             </Paper>
         </div>
     );
